497e3568db812af5982cd2486ff59286b27a426a
komatsuzaki-sogo <ougo.ks@gamil.com>
2022-07-27 22:55:34 +0900

    初期設定完了

---
+++ app/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const about = () => {
+  return (
+    <div className={styles.container}>about</div>
+  )
+}
+
+export default about
